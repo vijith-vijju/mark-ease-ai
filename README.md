a93039d8cd416a1fa6faae3657651d840b6e4bc6
vijith-vijju <vijithmylari27@gmail.com>
2026-08-30 22:14:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# mark-ease-ai
+# MarkEase
+
+AI-Powered Attendance Management System built using Face Recognition, Voice Recognition, Streamlit, and Supabase.
+
+## Features
+
+- Teacher & Student Authentication
+- Face Recognition Attendance
+- Voice Attendance Verification
+- Subject Management
+- Student Enrollment
+- Attendance Tracking & Analytics
+- Supabase Database Integration
+
+## Tech Stack
+
+- Python
+- Streamlit
+- Supabase
+- Face Recognition
+- Dlib
+- Resemblyzer
+- NumPy
+- Pandas
+- Scikit-Learn
+
+## Installation
+
+```bash
+git clone https://github.com/Gopal1909/markease-ai-attendance.git
+cd markease-ai-attendance
+pip install -r requirements.txt
+streamlit run app.py
+```
+
+## Screenshots
+
+### Home Page
+<img height="1030" alt="image" src="https://github.com/user-attachments/assets/7801da8d-2c9a-438c-ac67-cf532f64f5e5" />
+
+### Subject Management
+<img height="500" alt="image" src="https://github.com/user-attachments/assets/d56466bd-92d1-4291-b09e-df842b072d3a" /> 
+
+### Teacher Attendance Module
+<img height="500" alt="image" src="https://github.com/user-attachments/assets/9c8577cc-69be-4af9-b77e-62978aa0b129" />
+
+### Student Dashboard
+<img height="1030" alt="image" src="https://github.com/user-attachments/assets/1c8aa549-6f9b-44e7-946b-40adfac98e52" />
+
+
+## Author
+
+Gopal
---
+++ src/database/db.py
@@ -0,0 +1,122 @@
+from src.database.config import supabase
+import bcrypt
+
+
+
+def hash_pass(pwd):
+    return bcrypt.hashpw(pwd.encode(), bcrypt.gensalt()).decode()
+
+def check_pass(pwd, hashed):
+    return bcrypt.checkpw(pwd.encode(), hashed.encode())
+
+
+def check_teacher_exists(username):
+    # Check for unique username, returns false when username is already taken
+    response = supabase.table("teachers").select("username").eq("username", username).execute()
+    return len(response.data) > 0 
+
+
+
+def create_teacher(username, password, name):
+
+    data = { "username" : username, "password": hash_pass(password), "name": name}
+    response = supabase.table("teachers").insert(data).execute()
+    return response.data
+
+
+def teacher_login(username, password):
+    response = supabase.table("teachers").select("*").eq("username", username).execute()
+    if response.data:
+        teacher = response.data[0]
+        if check_pass(password, teacher['password']):
+            return teacher
+    return None
+
+
+def get_all_students():
+    response = supabase.table('students').select("*").execute()
+    return response.data
+
+def create_student(new_name, face_embedding=None, voice_embedding=None):
+    data = {'name': new_name, 'face_embedding':face_embedding, "voice_embedding": voice_embedding}
+    response = supabase.table('students').insert(data).execute()
+    return response.data
+
+
+def create_subject(subject_code, name, section, teacher_id):
+    data = {"subject_code": subject_code, "name": name, "section": section, "teacher_id": teacher_id}
+    response = supabase.table("subjects").insert(data).execute()
+    return response.data
+
+def get_teacher_subjects(teacher_id):
+    response = supabase.table('subjects').select("*, subject_students(count), attendance_logs(timestamp)").eq("teacher_id", teacher_id).execute()
+    subjects = response.data
+
+
+    for sub in subjects:
+        sub['total_students'] = sub.get("subject_students", [{}])[0].get('count', 0) if sub.get('subject_students') else 0
+        attendance = sub.get('attendance_logs', [])
+        unique_sessions = len(set(log['timestamp'] for log in attendance))
+        sub['total_classes'] = unique_sessions
+
+
+        sub.pop('subject_students', None)
+        sub.pop('attendance_logs', None)
+
+    return subjects
+
+
+def  enroll_student_to_subject(student_id, subject_id):
+    data = {'student_id': student_id, "subject_id": subject_id}
+    response= supabase.table('subject_students').insert(data).execute()
+    return response.data
+
+
+def  unenroll_student_to_subject(student_id, subject_id):
+    response= supabase.table('subject_students').delete().eq('student_id', student_id).eq('subject_id', subject_id).execute()
+    return response.data
+
+
+
+def get_student_subjects(student_id):
+    response = supabase.table('subject_students').select('*, subjects(*)').eq('student_id', student_id).execute()
+    return response.data
+
+
+def get_student_attendance(student_id):
+    response = supabase.table('attendance_logs').select('*, subjects(*)').eq('student_id', student_id).execute()
+    return response.data
+
+
+from datetime import date
+
+def create_attendance(logs):
+
+    today = date.today().isoformat()
+
+    valid_logs = []
+
+    for log in logs:
+
+        existing = (
+            supabase.table('attendance_logs')
+            .select('id')
+            .eq('student_id', log['student_id'])
+            .eq('subject_id', log['subject_id'])
+            .eq('attendance_date', today)
+            .execute()
+        )
+
+        if len(existing.data) == 0:
+            log['attendance_date'] = today
+            valid_logs.append(log)
+
+    if valid_logs:
+        response = supabase.table('attendance_logs').insert(valid_logs).execute()
+        return response.data
+
+    return []
+
+def get_attendance_for_teacher(teacher_id):
+    response = supabase.table('attendance_logs').select("*, subjects!inner(*)").eq('subjects.teacher_id', teacher_id).execute()
+    return response.data
